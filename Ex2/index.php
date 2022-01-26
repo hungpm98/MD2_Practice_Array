53f39bb776c63fb18df70e9a0cab9343f59c44e8
Hùng Phạm <hungpm.works@gmail.com>
2022-01-26 08:47:20 +0700

    update

--- Ex2/index.php
+++ Ex2/index.php
@@ -9,5 +9,38 @@ function createFloatArray($number)
     return $array;
 }
 
+$array1 = createFloatArray(10);
 echo "<pre>";
-print_r(createFloatArray(10));
+
+print_r($array1);
+
+function maxInArray($array)
+{
+    $max = $array[0];
+    for ($i = 0; $i < count($array); $i++) {
+        if ($array[$i] > $max) {
+            $max = $array[$i];
+        }
+    }
+    return $max;
+
+}
+echo "Max in array is: ";
+print_r(maxInArray($array1));
+
+
+function minInArray($array)
+{
+    $min = $array[0];
+    for ($i = 0; $i < count($array); $i++) {
+        if ($array[$i] < $min) {
+            $min = $array[$i];
+        }
+    }
+    return $min;
+
+}
+
+echo "<pre>";
+echo "Min in array is: ";
+print_r(minInArray($array1));
